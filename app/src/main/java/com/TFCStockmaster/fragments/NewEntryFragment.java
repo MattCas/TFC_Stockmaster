33ced22f62eb85b9e73b11108795af9b8acaffe2
MattCas <Matizzo.92>
2020-09-23 10:14:06 +0200

    Button listener working for manual entry

--- app/src/main/java/com/TFCStockmaster/fragments/NewEntryFragment.java
+++ app/src/main/java/com/TFCStockmaster/fragments/NewEntryFragment.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.TFCStockmaster.R;
 
@@ -14,50 +16,39 @@ import com.TFCStockmaster.R;
  * create an instance of this fragment.
  */
 public class NewEntryFragment extends Fragment {
-  // TODO: Rename parameter arguments, choose names that match
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-  private static final String ARG_PARAM1 = "param1";
-  private static final String ARG_PARAM2 = "param2";
-
-  // TODO: Rename and change types of parameters
-  private String mParam1;
-  private String mParam2;
 
   public NewEntryFragment() {
     // Required empty public constructor
   }
 
-  /**
-   * Use this factory method to create a new instance of
-   * this fragment using the provided parameters.
-   *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
-   * @return A new instance of fragment HomeFragment.
-   */
   // TODO: Rename and change types and number of parameters
   public static NewEntryFragment newInstance(String param1, String param2) {
     NewEntryFragment fragment = new NewEntryFragment();
     Bundle args = new Bundle();
-    args.putString(ARG_PARAM1, param1);
-    args.putString(ARG_PARAM2, param2);
-    fragment.setArguments(args);
     return fragment;
   }
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
+/*  //@Override
+  public void onCreateView(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     if (getArguments() != null) {
-      mParam1 = getArguments().getString(ARG_PARAM1);
-      mParam2 = getArguments().getString(ARG_PARAM2);
+
     }
-  }
+  }*/
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_new_entry, container, false);
+    View view = inflater.inflate(R.layout.fragment_new_entry, container, false);
+        final Button manualEntryButton = view.findViewById(R.id.manual_entry_button);
+        manualEntryButton.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+            Toast toast = Toast.makeText(getActivity(), "ManEntryButtonPressed", Toast.LENGTH_SHORT);
+            toast.show();
+          }
+        });
+    return view;
   }
 }
